36a751c8f175304d05778294a7c612421a3134ad
Raph <raph4sure007@gmail.com>
2025-04-12 13:19:07 +0100

    Add redux-thunk for asynchronous actions and enhance account operations with currency conversion

--- src/features/accounts/BalanceDisplay.js
+++ src/features/accounts/BalanceDisplay.js
@@ -1,12 +1,43 @@
+import { useSelector } from "react-redux";
+// import store from "../../store";
+
 function formatCurrency(value) {
+    return new Intl.NumberFormat("en", {
+        style: "currency",
+        currency: "USD",
+    }).format(value);
+}
+
+
+function BalanceDisplay() {
+    const { balance } = useSelector((store) => store.account);
+
+    return <div className="balance">{formatCurrency(balance)}</div>;
+}
+
+export default BalanceDisplay;
+
+
+
+// LEGACY OR old method
+
+/* import { connect } from "react-redux";
+
+ function formatCurrency(value) {
   return new Intl.NumberFormat("en", {
     style: "currency",
     currency: "USD",
   }).format(value);
 }
 
-function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+function BalanceDisplay({ balance }) {
+  return <div className="balance">{formatCurrency(balance)}</div>;
 }
 
-export default BalanceDisplay;
+function mapStateToProps(state) {
+  return {
+    balance: state.account.balance,
+  };
+}
+
+export default connect(mapStateToProps)(BalanceDisplay); */
